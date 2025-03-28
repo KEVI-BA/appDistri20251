2e5cc8cec1fccecc527d169dbf4de4087c5bfc8d
Kevin Barre <kevinbarre995@gmail.com>
2025-03-28 12:12:45 -0500

    Tarea de la WebApiPerson

---
+++ appEcommerceNetCore/WebApiPerson/WebApiPerson/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiPerson.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required int age { get; set; }
+    }
+}
